ec1b243b87acbfbc82d9a544d049fa0c7a109726
Avstrer <148279347+Avstrer@users.noreply.github.com>
2024-01-29 19:46:13 +0200

    end

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,32 @@
-function slugify (title) {
-    let toArray = title.split(' ');
-    let slug = (toArray.join('-')).toLowerCase();
-    return slug;
-}
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+function isEnoughCapacity(products, containerSize) {
+
+  const values = Object.keys(products);
+  
+  let total = 0;
+
+  for (const key of values) {
+  
+    total += products[key];
+    
+  }
+  
+  return true ? total <= containerSize : false;
+ }
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
+console.log(
+  isEnoughCapacity({ apples: 3, potatoes: 3, oranges: 1 }, 7)
+); // true
